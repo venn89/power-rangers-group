e1203bf1db8763f0e0a191997992f3d806487167
Lukas Ansteeg <l.w.ansteeg@uva.nl>
2024-09-03 10:10:13 +0200

    Update tests.py 2024 students

--- challenge_day2/tests/tests.py
+++ challenge_day2/tests/tests.py
@@ -6,9 +6,9 @@ import edutest
 
 class Test_Exercise_Values(edutest.TestCase):
     def test(self):
-        self.io_pair(('00:01', 5, '1990-01-01', 3, 1, 'Emma', 9, 'a+b'), 3.5190509710811696e+16)
-        self.io_pair(('00:02', 6, '1990-01-02', 4, 2, 'Emma', 8, 'b+c'), 1.6897595679400317e+17)
-        self.io_pair(('00:03', 7, '1990-01-03', 5, 3, 'Emma', 7, 'd*e'), 2.1272497207141005e+17)
+        self.io_pair(('00:01', 5, '1990-01-01', 3, 1, 'Alexandra', 9, 'a+b');1.50816470189193e+16)
+        self.io_pair(('00:02', 6, '1990-01-02', 4, 2, 'Alexandra', 8, 'b+c');7.241826719742992e+16)
+        self.io_pair(('00:03', 7, '1990-01-03', 5, 3, 'Alexandra', 7, 'd*e');9.116784517346144e+16)
 
 class Test_Exercise(edutest.TestSuite):
     test_cases = [
