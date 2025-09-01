20b5d8c4090473c9d0186caaa950789cfe1355a9
Betija Leoke <betijaleoke@gmail.com>
2025-09-01 10:38:51 +0200

    Added group exercise file that lists team members

---
+++ challenge_day1/git_exercise.py
@@ -0,0 +1,12 @@
+# Import teammates' files
+import betija
+import python
+import vanessas_file
+
+def team_intro():
+    print("This is Team Power Rangers. We are:")
+    print(betija.get_name())
+    print(python.get_name())
+    print(vanessas_file.get_name())
+
+
