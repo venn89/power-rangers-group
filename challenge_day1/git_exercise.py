699b36484bd602804ca9dfb0294338828295d916
Betija Leoke <betijaleoke@gmail.com>
2025-09-01 10:58:22 +0200

    This is a message describing what I did

---
+++ challenge_day1/git_exercise.py
@@ -0,0 +1,16 @@
+import betija
+import python   # ⚠️ consider renaming this file to avoid conflicts
+import vanessas_file
+import anan
+import Kiavash
+
+def team_intro():
+    print("This is Team PowerRangers. We are:")
+    print(betija.get_name())
+    print(python.get_name())
+    print(vanessas_file.get_name())
+    print(anan.get_name())
+    print(Kiavash.get_name())
+
+if __name__ == "__main__":
+    team_intro()
