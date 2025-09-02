70dc78f405a79a215b14fd813008cf0e59c372b5
Anna Vunsh <annavunsh@re-byods-145-109-60-66.wireless.uva.nl>
2025-09-02 10:23:42 +0200

    update station2

---
+++ challenge_day2/station2.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+def japanese_weekday(date_str: str) -> str:
+    weekdays = {
+        0: "月曜日",
+        1: "火曜日",
+        2: "水曜日",
+        3: "木曜日",
+        4: "金曜日",
+        5: "土曜日",
+        6: "日曜日"
+    }
+    date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+    weekday_num = date_obj.weekday()
+    return weekdays[weekday_num]
