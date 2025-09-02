e90c9e174df970fcbbc9fceaa50b28dee3a303a5
Lukas Ansteeg <l.w.ansteeg@uva.nl>
2025-09-02 11:23:30 +0200

    Self-test for day 2 challenge updated to new cohort

--- challenge_day2/tests/tests.py
+++ challenge_day2/tests/tests.py
@@ -6,9 +6,9 @@ import edutest
 
 class Test_Exercise_Values(edutest.TestCase):
     def test(self):
-        self.io_pair(('00:01', 5, '1990-01-01', 3, 1, 'Alexandra', 9, 'a+b'),1.50816470189193e+16)
-        self.io_pair(('00:02', 6, '1990-01-02', 4, 2, 'Alexandra', 8, 'b+c'),7.241826719742992e+16)
-        self.io_pair(('00:03', 7, '1990-01-03', 5, 3, 'Alexandra', 7, 'd*e'),9.116784517346144e+16)
+        self.io_pair(('00:01', 5, '1990-01-01', 3, 1, 'Maliah', 9, 'a+b'),5027215672973100.0)
+        self.io_pair(('00:02', 6, '1990-01-02', 4, 2, 'Maliah', 8, 'b+c'),2.413942239914331e+16)
+        self.io_pair(('00:03', 7, '1990-01-03', 5, 3, 'Maliah', 7, 'd*e'),3.038928172448715e+16)
 
 class Test_Exercise(edutest.TestSuite):
     test_cases = [
