55b08e39a7da7bf2ee510a6047068b0266ee795e
Vanessa Yan <vanessayan89@gmail.com>
2025-09-02 10:48:02 +0200

    ya

--- challenge_day2/station4.py
+++ challenge_day2/station4.py
@@ -1,11 +1,7 @@
 import math
 
 def solution_station_4(x):
-    status = 1
     for i in range(2, int(math.sqrt(x)) + 1):
         if x % i == 0:
-            status = 0
-            break
-        else:
-            continue
-    return status
+            return "False"
+    return "True"
