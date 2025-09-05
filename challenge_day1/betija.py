bc8603b59f2fa458eb98f068441454ca0056946d
Anna Vunsh <annavunsh@Annas-MacBook-Air-3.local>
2025-09-05 14:04:21 +0200

    update b

--- challenge_day1/betija.py
+++ challenge_day1/betija.py
@@ -3,6 +3,6 @@ def get_name():
 
 paragraphs = [
     "Beti told Anna, Nohemi, Kiavash, Vanessa, and Andre a legend about such a device opening hidden worlds. Her story made Nohemi nervous, Anna thoughtful, and Andre suspicious.",
-    "Beti led Anna, Nohemi, Kiavash, Vanessa, and Andre through the ruins, feeling alive in the unknown. Strange shadows loomed with intent among the trees.",
+    "Beti led Anna, Nohemi, Kiavash, Vanessa, and Andre through the ruins, feeling alive and free in the unknown. Strange shadows loomed with intent among the trees.",
     "Beti bravely distracted the guardian so Anna, Nohemi, Kiavash, Vanessa, and Andre could act. Her fearless grin gave the group a chance to regain control."
 ]
