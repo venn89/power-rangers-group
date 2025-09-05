271ae0931f78626da23a4a3918922f23ea98861b
nohemihm <nohemiiihm@outlook.com>
2025-09-05 13:05:16 +0200

    update

--- challenge_day1/Kiavash.py
+++ challenge_day1/Kiavash.py
@@ -3,6 +3,6 @@ def get_name():
 
 paragraphs = [
     "Kiavash showed Anna, Beti, Nohemi, Vanessa, and Andre ancient notes linking the device to portals.The group argued fiercely, torn between wonder and fear.",
-    "Kiavash read carvings to Anna, Beti, Nohemi, Vanessa, and Andre, revealing that betrayal awaited their group. The prophecy shook their trust in one another.",
-    "Kiavash realized the device could close the portal,explaining it to Anna, Beti, Nohemi, Vanessa, and Andre. Though torn, they agreed to return home."
+    "Kiavash read carvings to Anna, Beti, Nohemi, Vanessa, and Andre, uncovering that betrayal awaited their group. The prophecy shook their trust in one another.",
+    "Kiavash realized the device could shut the portal down ,explaining it to Anna, Beti, Nohemi, Vanessa, and Andre. Though torn, they agreed to return home."
 ]
