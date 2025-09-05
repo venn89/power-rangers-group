503146e2d0c868d655b43fdb3e604f842cf76c80
Kiavash Asadi <kia.asadi.1380@gmail.com>
2025-09-05 13:16:20 +0200

    Updated andre.py with new story paragraphs

--- challenge_day1/andre.py
+++ challenge_day1/andre.py
@@ -4,7 +4,7 @@ def get_name():
 paragraphs = [
     "Andre described dreams of the six—Anna, Beti, Nohemi, Kiavash, and Vanessa—standing at a shining doorway. His visions made the device glow brighter in Anna’s hands.",
     "Andre guided Anna, Beti, Nohemi, Kiavash, and Vanessa to a hidden temple he had seen in his dreams. The walls shimmered as the device pulsed in Anna’s grip.",
-    "As the portal closed, Andre saw his dream fulfilled with Anna, Beti, Nohemi, Kiavash, and Vanessa at his side. Together they awoke in Halewick, forever changed."]
+    "When the portal closed, Andre saw his dream fulfilled with Anna, Beti, Nohemi, Kiavash, and Vanessa at his side. Together they awoke in Halewick, forever changed."]
 
 
 
