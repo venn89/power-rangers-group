1bb62eb2d5c40d10a3ed62a93955c7d318c1f2b0
Kiavash Asadi <kia.asadi.1380@gmail.com>
2025-09-05 12:45:06 +0200

    Updated Kiavash.py with new story paragraphs

--- challenge_day1/Kiavash.py
+++ challenge_day1/Kiavash.py
@@ -1,2 +1,8 @@
 def get_name():
     return "Kiavash"
+
+paragraphs = [
+    "Kiavash showed Anna, Beti, Nohemi, Vanessa, and Andre ancient notes linking the device to portals.The group argued fiercely, torn between wonder and fear.",
+    "Kiavash read carvings to Anna, Beti, Nohemi, Vanessa, and Andre, revealing that betrayal awaited their group. The prophecy shook their trust in one another.",
+    "Kiavash realized the device could close the portal,explaining it to Anna, Beti, Nohemi, Vanessa, and Andre. Though torn, they agreed to return home."
+]
