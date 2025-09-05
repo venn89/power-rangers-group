b7a63b36c773c137e82dd725a639a0aa01abd066
nohemihm <nohemiiihm@outlook.com>
2025-09-05 13:09:14 +0200

    changes

--- challenge_day1/Vanessa.py
+++ challenge_day1/Vanessa.py
@@ -4,6 +4,6 @@ def get_name():
 
 paragraphs = [
     "Vanessa suggested secretly testing the device with Anna, Beti, Nohemi, Kiavash, and Andre before anyone else knew. Her sly grin made Nohemi suspicious, but Kiavash admitted the idea was tempting.",
-    "Vanessa considered stealing the device from Anna, Beti, Nohemi, Kiavash, and Andre, though she stayed silent. Her shifting eyes made Andre uneasy, as if he already knew.",
+    "Vanessa considered taking the device from Anna, Beti, Nohemi, Kiavash, and Andre, though she stayed silent. Her shifting eyes made Andre uneasy, as if he already knew.",
     "Vanessa confessed to Anna, Beti, Nohemi, Kiavash, and Andre that she had thought of betrayal but chose loyalty. Her honesty surprised the others and eased their fears."
 ]
