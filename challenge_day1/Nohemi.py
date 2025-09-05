4e90062c89457219bc6abc2087e37899f1d2b09a
Anna Vunsh <annavunsh@Annas-MacBook-Air-3.local>
2025-09-05 14:07:06 +0200

    update n

--- challenge_day1/Nohemi.py
+++ challenge_day1/Nohemi.py
@@ -2,7 +2,7 @@ def get_name():
     return "Nohemi"
 
 paragraphs = [
-   "Nohemi warned Anna, Beti, Kiavash, Vanessa, and Andre that power like this often led to disaster. Her words weighed heavily, though Anna defended her invention with pridege",
+   "Nohemi warned Anna, Beti, Kiavash, Vanessa, and Andre that power like this often led to disaster. Her words weighed heavily, though Anna defended her invention with pleasure",
    "Nohemi healed a wounded beast while Anna, Beti, Kiavash, Vanessa, and Andre watched warily. The creature warned them of a guardian deeper within.",
    "Nohemi healed Anna’s wounds as Beti, Kiavash, Vanessa, and Andre stood guard. Her sacrifice drained her but strengthened their unity"
 
