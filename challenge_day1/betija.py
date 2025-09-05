00c22e3b4e861143d2e79a014de7c8571f73bec5
Betija Leoke <betijaleoke@gmail.com>
2025-09-05 12:13:12 +0200

    betija changes

--- challenge_day1/betija.py
+++ challenge_day1/betija.py
@@ -1,2 +1,8 @@
 def get_name():
     return "Betija"
+
+paragraphs = [
+    "Beti told Anna, Nohemi, Kiavash, Vanessa, and Andre a legend about such a device opening hidden worlds. Her story made Nohemi nervous, Anna thoughtful, and Andre suspicious.",
+    "Beti led Anna, Nohemi, Kiavash, Vanessa, and Andre through the ruins, feeling alive in the unknown. Strange shadows moved with intent among the trees.",
+    "Beti bravely distracted the guardian so Anna, Nohemi, Kiavash, Vanessa, and Andre could act. Her fearless grin gave the group a chance to regain control."
+]
