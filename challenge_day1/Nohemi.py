65bef3480f89b9925c108e681c009bf4cfd7ff3a
Betija Leoke <betijaleoke@gmail.com>
2025-09-05 13:02:53 +0200

    fixed Noemi to Nohemi

--- challenge_day1/Nohemi.py
+++ challenge_day1/Nohemi.py
@@ -2,8 +2,8 @@ def get_name():
     return "Nohemi"
 
 paragraphs = [
-   "Noemi warned Anna, Beti, Kiavash, Vanessa, and Andre that power like this often led to disaster. Her words weighed heavily, though Anna defended her invention with pridege",
-   "Noemi healed a wounded beast while Anna, Beti, Kiavash, Vanessa, and Andre watched warily. The creature warned them of a guardian deeper within.",
-   "Noemi healed Anna’s wounds as Beti, Kiavash, Vanessa, and Andre stood guard. Her sacrifice drained her but strengthened their unity"
+   "Nohemi warned Anna, Beti, Kiavash, Vanessa, and Andre that power like this often led to disaster. Her words weighed heavily, though Anna defended her invention with pridege",
+   "Nohemi healed a wounded beast while Anna, Beti, Kiavash, Vanessa, and Andre watched warily. The creature warned them of a guardian deeper within.",
+   "Nohemi healed Anna’s wounds as Beti, Kiavash, Vanessa, and Andre stood guard. Her sacrifice drained her but strengthened their unity"
 
 ]
