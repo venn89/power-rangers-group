b3aa44565538cffa8e92c1916b02494a32a9adcb
Anna Vunsh <annavunsh@Annas-MacBook-Air-3.local>
2025-09-05 12:03:30 +0200

    update

--- challenge_day1/anan.py
+++ challenge_day1/anan.py
@@ -1,3 +1,8 @@
-def get_name():
-    return"Anna"
+paragraphs = [
+    "In Halewick, Anna revealed a glowing device to Beti, Nohemi, Kiavash, Vanessa, and Andre, claiming it could change their future. The strange hum unsettled them all, though excitement flickered in Kiavash’s eyes.",
+    "When Anna activated the device, Beti, Nohemi, Kiavash, Vanessa, and Andre were hurled into a twisted forest. The world around them pulsed with ancient energy.",
+    "Inside the temple, Anna puzzled over the guardian’s riddles while Beti, Nohemi, Kiavash, Vanessa, and Andre supported her. With Andre’s whispered hint, she finally found the answer."
+]
 
+def get_name():
+    return "Anna"
