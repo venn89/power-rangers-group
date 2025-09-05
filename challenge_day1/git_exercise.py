da53c238592f1c89fe5f60665418db3328c23c8f
Vanessa Yan <vanessayan89@gmail.com>
2025-09-05 12:20:26 +0200

    ya

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,22 +1,22 @@
-import anna
-import beti
-import nohemi
-import kiavash
-import vanessa
+import anan
+import betija
+import Nohemi
+import Kiavash
+import Vanessa
 import andre
 
 def team_intro():
     print("This is Team PowerRangers. We are:")
     print(anan.get_name())
-    print(beti.get_name())
-    print(nohemi.get_name())
-    print(kiavash.get_name())
-    print(vanessa.get_name())
+    print(betija.get_name())
+    print(Nohemi.get_name())
+    print(Kiavash.get_name())
+    print(Vanessa.get_name())
     print(andre.get_name())
 
 def story():
     # Only Anna's paragraphs
-    for p in anna.paragraphs:
+    for p in anan.paragraphs:
         print(p)
 
 if __name__ == "__main__":
