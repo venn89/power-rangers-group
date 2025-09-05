a43c55282f5ed05074a900261b8180be64ab233d
nohemihm <nohemiiihm@outlook.com>
2025-09-05 12:28:51 +0200

    nohemi file

--- challenge_day1/Nohemi.py
+++ challenge_day1/Nohemi.py
@@ -1,2 +1,9 @@
 def get_name():
     return "Nohemi"
+
+paragraphs = [
+   "Noemi warned Anna, Beti, Kiavash, Vanessa, and Andre that power like this often led to disaster. Her words weighed heavily, though Anna defended her invention with pridege",
+   "Noemi healed a wounded beast while Anna, Beti, Kiavash, Vanessa, and Andre watched warily. The creature warned them of a guardian deeper within.",
+   "Noemi healed Anna’s wounds as Beti, Kiavash, Vanessa, and Andre stood guard. Her sacrifice drained her but strengthened their unity"
+
+]
