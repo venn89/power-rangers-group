6c606c900f58761c5d34cd3d4293dafa664e6a67
Anna Vunsh <annavunsh@Annas-MacBook-Air-3.local>
2025-09-05 12:04:44 +0200

    update

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,18 +1,24 @@
-import betija
-import Nohemi   
-import Vanessa
-import anan
-import Kiavash
+import anna
+import beti
+import nohemi
+import kiavash
+import vanessa
 import andre
 
 def team_intro():
     print("This is Team PowerRangers. We are:")
-    print(betija.get_name())
-    print(Nohemi.get_name())
-    print(Vanessa.get_name())
     print(anan.get_name())
-    print(Kiavash.get_name())
+    print(beti.get_name())
+    print(nohemi.get_name())
+    print(kiavash.get_name())
+    print(vanessa.get_name())
     print(andre.get_name())
+
+def story():
+    # Only Anna's paragraphs
+    for p in anna.paragraphs:
+        print(p)
 
 if __name__ == "__main__":
     team_intro()
+    story()
