9f4cd0b7b98198f12bc45f7a189d9f9a9b53b06c
Andre <dydylk9@gmail.com>
2025-09-05 11:34:10 +0200

    reformatted

--- challenge_day1/andre.py
+++ challenge_day1/andre.py
@@ -2,13 +2,17 @@
 def get_name():
     return "Andre"
 
-def paragraph6():
-    return "Andre described dreams of the six—Anna, Beti, Nohemi, Kiavash, and Vanessa—standing at a shining doorway. His visions made the device glow brighter in Anna’s hands."
+def story_para6():
+    print("Paragraph 6 (Andre's file)")
+    print("Andre described dreams of the six—Anna, Beti, Nohemi, Kiavash, and Vanessa—standing at a shining doorway. His visions made the device glow brighter in Anna’s hands.")   return 
 
-def paragraph12():
-    return "Andre guided Anna, Beti, Nohemi, Kiavash, and Vanessa to a hidden temple he had seen in his dreams. The walls shimmered as the device pulsed in Anna’s grip."
+def story_para12():
+    print("Paragraph 12 (Andre's file)")
+    print("Andre guided Anna, Beti, Nohemi, Kiavash, and Vanessa to a hidden temple he had seen in his dreams. The walls shimmered as the device pulsed in Anna’s grip.")
+    return 
 
-def paragraph18():
-    return "As the portal closed, Andre saw his dream fulfilled with Anna, Beti, Nohemi, Kiavash, and Vanessa at his side. Together they awoke in Halewick, forever changed."
+def story_para18():
+    print("Paragraph 18 (Andre's file)")
+    print("As the portal closed, Andre saw his dream fulfilled with Anna, Beti, Nohemi, Kiavash, and Vanessa at his side. Together they awoke in Halewick, forever changed.")
+    return 
 
-print(paragraph12())
